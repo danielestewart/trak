6715ec8f1223ec4d5d7792addd9e4dd51cc62757
Daniel Stewart <danielst@mit.edu>
2023-04-27 14:12:49 -0400

    add default cat

--- trak/modelout_functions.py
+++ trak/modelout_functions.py
@@ -107,7 +107,7 @@ class ImageClassificationModelOutput(AbstractModelOutput):
                    buffers: Iterable[Tensor],
                    image: Tensor,
                    label: Tensor,
-                   category: int) -> Tensor:
+                   category: int = 0) -> Tensor:
         """ For a given input :math:`z=(x, y)` and model parameters :math:`\\theta`,
         let :math:`p(z, \\theta)` be the softmax probability of the correct class.
         This method implements the model output function
@@ -181,7 +181,7 @@ class ImageClassificationModelOutput(AbstractModelOutput):
         images, _ = batch
         return model(images)
 
-    def get_out_to_loss_grad(self, func_model, weights, buffers, batch: Iterable[Tensor], category: int) -> Tensor:
+    def get_out_to_loss_grad(self, func_model, weights, buffers, batch: Iterable[Tensor], category: int = 0) -> Tensor:
         """ Computes the (reweighting term Q in the paper)
 
         Args:
