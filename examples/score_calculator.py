2004ba008bfd7ce481e5d1a03916064e0db7356f
Daniel Stewart <danielst@mit.edu>
2023-05-15 01:28:43 -0400

    added score_calculator

---
+++ examples/score_calculator.py
@@ -0,0 +1,184 @@
+import os
+from pathlib import Path
+import wget
+from tqdm.auto import tqdm
+import numpy as np
+import torch
+from torch.cuda.amp import GradScaler, autocast
+from torch.nn import CrossEntropyLoss, Conv2d, BatchNorm2d
+from torch.optim import SGD, lr_scheduler
+import torchvision
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# Resnet9
+class Mul(torch.nn.Module):
+    def __init__(self, weight):
+        super(Mul, self).__init__()
+        self.weight = weight
+    def forward(self, x): return x * self.weight
+
+
+class Flatten(torch.nn.Module):
+    def forward(self, x): return x.view(x.size(0), -1)
+
+
+class Residual(torch.nn.Module):
+    def __init__(self, module):
+        super(Residual, self).__init__()
+        self.module = module
+    def forward(self, x): return x + self.module(x)
+
+
+def construct_rn9(num_classes=10):
+    def conv_bn(channels_in, channels_out, kernel_size=3, stride=1, padding=1, groups=1):
+        return torch.nn.Sequential(
+                torch.nn.Conv2d(channels_in, channels_out, kernel_size=kernel_size,
+                            stride=stride, padding=padding, groups=groups, bias=False),
+                torch.nn.BatchNorm2d(channels_out),
+                torch.nn.ReLU(inplace=True)
+        )
+    model = torch.nn.Sequential(
+        conv_bn(3, 64, kernel_size=3, stride=1, padding=1),
+        conv_bn(64, 128, kernel_size=5, stride=2, padding=2),
+        Residual(torch.nn.Sequential(conv_bn(128, 128), conv_bn(128, 128))),
+        conv_bn(128, 256, kernel_size=3, stride=1, padding=1),
+        torch.nn.MaxPool2d(2),
+        Residual(torch.nn.Sequential(conv_bn(256, 256), conv_bn(256, 256))),
+        conv_bn(256, 128, kernel_size=3, stride=1, padding=0),
+        torch.nn.AdaptiveMaxPool2d((1, 1)),
+        Flatten(),
+        torch.nn.Linear(128, num_classes, bias=False),
+        Mul(0.2)
+    )
+    return model
+
+def get_dataloader(batch_size=256, num_workers=8, split='train', shuffle=False, augment=True):
+    if augment:
+        transforms = torchvision.transforms.Compose(
+                        [torchvision.transforms.RandomHorizontalFlip(),
+                         torchvision.transforms.RandomAffine(0),
+                         torchvision.transforms.ToTensor(),
+                         torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                                          (0.2023, 0.1994, 0.201))])
+    else:
+        transforms = torchvision.transforms.Compose([
+                         torchvision.transforms.ToTensor(),
+                         torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                                          (0.2023, 0.1994, 0.201))])
+        
+    is_train = (split == 'train')
+    dataset = torchvision.datasets.CIFAR10(root='/tmp/cifar/',
+                                           download=True,
+                                           train=is_train,
+                                           transform=transforms)
+
+    loader = torch.utils.data.DataLoader(dataset=dataset,
+                                         shuffle=shuffle,
+                                         batch_size=batch_size,
+                                         num_workers=num_workers)
+    
+    return loader
+
+def train(model, loader, lr=0.4, epochs=24, momentum=0.9,
+          weight_decay=5e-4, lr_peak_epoch=5, label_smoothing=0.0, model_id=0):
+    
+    opt = SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+    iters_per_epoch = len(loader)
+    # Cyclic LR with single triangle
+    lr_schedule = np.interp(np.arange((epochs+1) * iters_per_epoch),
+                            [0, lr_peak_epoch * iters_per_epoch, epochs * iters_per_epoch],
+                            [0, 1, 0])
+    scheduler = lr_scheduler.LambdaLR(opt, lr_schedule.__getitem__)
+    scaler = GradScaler()
+    loss_fn = CrossEntropyLoss(label_smoothing=label_smoothing)
+
+    for ep in range(epochs):
+        for it, (ims, labs) in enumerate(loader):
+            ims = ims.cuda()
+            labs = labs.cuda()
+            opt.zero_grad(set_to_none=True)
+            with autocast():
+                out = model(ims)
+                loss = loss_fn(out, labs)
+
+            scaler.scale(loss).backward()
+            scaler.step(opt)
+            scaler.update()
+            scheduler.step()
+        if ep in [12, 15, 18, 21, 23]:
+            torch.save(model.state_dict(), f'./checkpoints/sd_{model_id}_epoch_{ep}.pt')
+        
+    return model
+
+ckpt_files = sorted(list(Path('./checkpoints').rglob('*.pt')))
+ckpts = [torch.load(ckpt, map_location='cpu') for ckpt in ckpt_files]
+
+model = construct_rn9().to(memory_format=torch.channels_last).cuda()
+model.load_state_dict(ckpts[-1])
+model = model.eval()
+
+import sys
+sys.path.insert(0, '..')
+import trak
+from trak import TRAKer
+from trak import modelout_functions
+
+loader = get_dataloader(split='val', augment=False)
+model.eval()
+
+with torch.no_grad():
+    total_correct, total_num = 0., 0.
+    for ims, labs in tqdm(loader):
+        ims = ims.cuda()
+        labs = labs.cuda()
+        with autocast():
+            out = model(ims)
+            print(out.size())
+            total_correct += out.argmax(1).eq(labs).sum().cpu().item()
+            total_num += ims.shape[0]
+
+    print(f'Accuracy: {total_correct / total_num * 100:.1f}%')
+
+batch_size = 128
+loader_train = get_dataloader(batch_size=batch_size, split='train')
+
+def l_inf_norm(arr):
+    return max(np.fabs(arr))
+
+arr_of_scores = torch.empty((1,10), dtype = torch.float64)
+from trak.projectors import CudaProjector 
+from trak.projectors import ProjectionType
+a = sum([x.numel() for x in model.parameters()])
+
+for category in range(10):
+    traker = TRAKer(model=model,
+                task='image_classification_cat',
+                proj_dim=4096,
+                train_set_size=len(loader_train.dataset),
+                category=category,
+                projector = CudaProjector(a, 4096, 0, ProjectionType.rademacher, 'cuda', max_batch_size = 8))
+
+    for model_id, ckpt in enumerate(tqdm(ckpts)):
+        traker.load_checkpoint(ckpt, model_id=model_id)
+        for batch in tqdm(loader_train):
+            batch = [x.cuda() for x in batch]
+            traker.featurize(batch=batch, num_samples=batch[0].shape[0])
+
+    traker.finalize_features()
+    for model_id, ckpt in enumerate(tqdm(ckpts)):
+        traker.start_scoring_checkpoint(ckpt,
+                                    model_id=model_id,
+                                    num_targets=len(loader_targets.dataset))
+    for batch in loader_targets:
+        batch = [x.cuda() for x in batch]
+        traker.score(batch=batch, num_samples=batch[0].shape[0])
+
+    scores = traker.finalize_scores().cpu()
+    arr_of_scores[0][category] = modelout_functions.get_infty_norm(scores)
+
+    torch.save(arr_of_scores, 'first' + category + 'categories.pt')
+    
+    
+    
